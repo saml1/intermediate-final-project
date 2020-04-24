ecc7d04d6914157260cd5f7cdba6616d307dbeae
Sam Lipschitz <slipsch3@jhu.edu>
2020-04-24 10:49:09 -0400

    added wall.cpp

---
+++ wall.cpp
@@ -0,0 +1,21 @@
+#include "wall.h"
+
+Wall::Wall(){
+
+}
+
+Wall::~Wall(){
+
+}
+
+Wall::MoveResult checkMoveOnto(Entity *entity, const Position &fromPos, const Position &tilePos) const{
+
+}
+
+Wall::bool isGoal() const{
+
+}
+
+Wall::std::string getGlyph() const{
+
+}
