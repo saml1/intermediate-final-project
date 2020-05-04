aa401ad3408ae770f84a1da5b549096f5f0c6ea0
Brayden Archer <barcher9@jh.edu>
2020-05-03 22:33:37 -0400

    Worked on basicgamerules, almost done

--- basicgamerules.cpp
+++ basicgamerules.cpp
@@ -1,5 +1,6 @@
 #include "basicgamerules.h"
 #include "gamerules.h"
+#include "maze.h"
 
 BasicGameRules::BasicGameRules() {}
 
@@ -7,7 +8,30 @@ BasicGameRules::~BasicGameRules() {}
 
 
 bool BasicGameRules::allowMove(Game *game, Entity *actor, const Position &source, const Position &dest) const {
-  return false;
+  if(source.distanceFrom(dest) > 1) { //Make sure move isnt more than 1 position
+    return false;
+  }
+  Maze * maze = game->getMaze();
+  int width = maze->getWidth();
+  int height = maze->getHeight();
+  if(!dest.inBounds(width, height)) {
+    return false;
+  }
+  
+  Entity * eAtD = game->getEntityAt(dest);
+  const Tile * tileD = maze->getTile(dest);
+  if(eAtD == NULL) { //Check is there is an entity
+    if(tileD->checkMoveOnto(actor, source, dest) == MoveResult::BLOCK) {
+      return false;
+    }
+  }
+  else {
+    
+  }
+  
+
+
+  return true;
 }
 
 void BasicGameRules::enactMove(Game *game, Entity *actor, const Position &dest) const {
