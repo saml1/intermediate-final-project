8b86911839b899275c825d4a978fa1ad19efac1b
Sean O'C <soconn19@jhu.edu>
2020-05-06 07:10:54 -0400

    Coded chasehero but haven't compiled or tested it

--- chasehero.cpp
+++ chasehero.cpp
@@ -6,7 +6,60 @@ ChaseHero::ChaseHero() {}
 ChaseHero::~ChaseHero() {}
 
 Direction ChaseHero::getMoveDirection(Game *game, Entity *entity) {
-  return Direction::NONE; //Stub
+
+  // The section below determines which hero to chase
+  // It is unfortunately reused code from AStarChaseHero
+  // but I couldn't think of a simpler way to implement it
+
+  std::vector<Entity *> heroVector = game->getEntitiesWithProperty('h');
+
+  if (heroVector.empty()) return Direction::NONE;
+
+  Entity * hero = heroVector.front();
+
+  for (std::vector<Entity *>::iterator it = heroVector.begin(); it != heroVector.end(); ++it) {
+    if ((*it)->getPosition().distanceFrom(start) < hero->getPosition().distanceFrom(start))
+      hero = (*it);
+  }
+
+  // Determining which direction to move in
+
+  Position start = entity->getPosition();
+
+  Position goal = hero->getPosition();
+
+  int horizontal = start.getX() - goal.getX();
+  Direction hor = Direction::LEFT;
+  if (horizontal < 0) {
+    horizontal *= -1;
+    hor = Direction::RIGHT;
+  }
+  
+  int vertical = start.getY() - goal.getY();
+  Direction ver = Direction::DOWN;
+  if (vertical < 0) {
+    vertical *= -1;
+    ver = Direction::UP;
+  }
+
+  if (horizontal >= vertical) {
+    step = start.displace(hor);
+    if (maze.inBounds(step) && maze.getTile(step)->checkMoveOnto(entity, start, step))
+      return hor;
+    step = start.displace(ver);
+    if (maze.inBounds(step) && maze.getTile(step)->checkMoveOnto(entity, start, step))
+      return ver;
+  }
+  else {
+    step = start.displace(ver);
+    if (maze.inBounds(step) && maze.getTile(step)->checkMoveOnto(entity, start, step))
+      return ver;
+    step = start.displace(hor);
+    if (maze.inBounds(step) && maze.getTile(step)->checkMoveOnto(entity, start, step))
+      return hor;
+  }
+
+  return Direction::NONE;
 }
 
 bool ChaseHero::isUser() const {
